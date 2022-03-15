fc6210598d659a859cb5c1d0fd42b088dd90b41f
Daniel Gallichan <dgallichan@yahoo.com>
2022-03-15 10:32:44 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let list: number[] = []
 function sumArray (list: any[]) {
     valueSum = 0
     for (let value of list) {
-        valueSum = valueSum + list[list.length]
+        valueSum = valueSum + value
     }
     return valueSum
 }
@@ -16,12 +16,11 @@ let readingsList = [
 0,
 0
 ]
-valueSum = input.acceleration(Dimension.X)
 basic.forever(function () {
     readingsList.shift()
     readingsList.push(input.acceleration(Dimension.X))
     averagedValue = sumArray(readingsList) / windowSize
-    serial.writeValue("x.ax", readingsList[readingsList.length])
+    serial.writeValue("x.ax", readingsList[readingsList.length - 1])
     serial.writeValue("x.ax_avg", averagedValue)
     basic.pause(50)
 })
