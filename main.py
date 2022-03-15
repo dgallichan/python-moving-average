c3b6bf873a5bfa32f0f6d5124495974d9199c0ca
Daniel Gallichan <dgallichan@yahoo.com>
2022-03-15 10:26:10 +0000

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+averagedValue = 0
 windowSize = 5
 readingsList = [0, 0, 0, 0, 0]
 valueSum = input.acceleration(Dimension.X)
 
 def on_forever():
-    global valueSum
-    valueSum = valueSum - readingsList[len(readingsList)]
+    global valueSum, averagedValue
+    valueSum = valueSum - readingsList[0]
     readingsList.append(input.acceleration(Dimension.X))
-    
+    valueSum = valueSum + readingsList[len(readingsList)]
+    averagedValue = valueSum / windowSize
+    serial.write_value("x.ax", readingsList[len(readingsList)])
+    serial.write_value("x.ax_avg", averagedValue)
+    basic.pause(50)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+let list: number[] = []
+function sumArray (list: any[]) {
+    valueSum = 0
+    for (let value of list) {
+        valueSum = valueSum + list[list.length]
+    }
+    return valueSum
+}
 let averagedValue = 0
+let valueSum = 0
 let windowSize = 5
 let readingsList = [
 0,
@@ -7,12 +16,11 @@ let readingsList = [
 0,
 0
 ]
-let valueSum = input.acceleration(Dimension.X)
+valueSum = input.acceleration(Dimension.X)
 basic.forever(function () {
-    valueSum = valueSum - readingsList[0]
+    readingsList.shift()
     readingsList.push(input.acceleration(Dimension.X))
-    valueSum = valueSum + readingsList[readingsList.length]
-    averagedValue = valueSum / windowSize
+    averagedValue = sumArray(readingsList) / windowSize
     serial.writeValue("x.ax", readingsList[readingsList.length])
     serial.writeValue("x.ax_avg", averagedValue)
     basic.pause(50)
